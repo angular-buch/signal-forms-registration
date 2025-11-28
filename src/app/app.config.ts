a4f6500c6544a74d6a20bbbf76113a789e0dc811
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2025-11-28 21:42:44 +0100

    feat: use provideSignalFormsConfig()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideSignalFormsConfig } from '@angular/forms/signals';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideSignalFormsConfig({
+        classes: {
+            valid: (state) => state.valid(),
+            error: (state) => state.touched() && state.errors().length > 0,
+            pending: (state) => state.pending(),
+        },
+    }),
+  ],
 };
