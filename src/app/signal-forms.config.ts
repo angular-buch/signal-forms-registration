0b2d13af2f662d9f0181e9ebd4e06f90cdf1dd01
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2025-11-29 10:51:24 +0100

    refactor: add NG_STATUS_CLASSES

--- src/app/signal-forms.config.ts
+++ src/app/signal-forms.config.ts
@@ -1,9 +1,9 @@
 import { SignalFormsConfig } from '@angular/forms/signals';
+import { NG_STATUS_CLASSES } from '@angular/forms/signals/compat';
 
 export const signalFormsConfig: SignalFormsConfig = {
   classes: {
-    valid: (state) => state.valid(),
+    ...NG_STATUS_CLASSES,
     error: (state) => state.touched() && state.errors().length > 0,
-    pending: (state) => state.pending(),
   },
 };
